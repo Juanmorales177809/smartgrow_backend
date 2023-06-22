5686cb88a4cf9c51953a46bd427bc6f999dee0b2
santiagoSuarez219 <santiago8628@gmail.com>
2023-06-22 12:20:30 -0500

    Toggle Rele

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ const { createServer } = require('http');
 const realtimeServer = require('./realtimeServer');
 const path = require('path');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const httpServer = createServer(app);
 
+
+app.use(cors());
 app.set('port', process.env.PORT || 8000);
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -34,6 +37,31 @@ app.post('/hidroponico', (req, res) =>{
     const humedad = req.body.humedad;
     io.emit('message', {sensor: 'hidroponico', ph: ph, ec: ec, temperatura: temperatura, humedad: humedad});
     res.status(200).json({message: 'recibido'})
+});
+
+app.post('/sensores_de_flujo', (req, res) =>{
+    const flujo_1 = req.body.flujo_1;
+    const flujo_2 = req.body.flujo_2;
+    const flujo_3 = req.body.flujo_3;
+    const litros_1 = req.body.litros_1;
+    const litros_2 = req.body.litros_2;
+    const litros_3 = req.body.litros_3;
+    const temperatura_agua1 = req.body.temperatura_agua1;
+    const temperatura_agua2 = req.body.temperatura_agua2;
+    const temperatura_agua3 = req.body.temperatura_agua3;
+    console.log(flujo_1, flujo_2, flujo_3, litros_1, litros_2, litros_3, temperatura_agua1, temperatura_agua2, temperatura_agua3);
+    res.status(200).json({message: 'recibido'});
+});
+
+app.get('/rele', async (req, res) =>{
+    try {
+        const response = await fetch("http://10.220.85.101/api?rele=rele1_pin1");
+        const data = await response.json();
+        console.log(data);
+        res.json(data)
+      } catch (error) {
+        console.log(error);
+      }
 });
 
 httpServer.listen(app.get('port'), () => {
