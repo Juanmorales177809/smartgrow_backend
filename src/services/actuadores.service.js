048600a254491ae37e7b088327e23c4e5bf83ec9
santiagoSuarez219 <santiago8628@gmail.com>
2023-07-21 17:51:44 -0500

    reles ph, npk, micro, at

--- src/services/actuadores.service.js
+++ src/services/actuadores.service.js
@@ -2,9 +2,9 @@ class ActuadoresService{
     constructor(){};
 
     async activation(rele, clientMqtt, timeRecirculacion = 30000){
-        if (rele == 'entrada_de_agua_hidroponico' || rele == 'desague_hidroponico'){
+        if (rele == 'entrada_de_agua_hidroponico' || rele == 'desague_hidroponico' || rele == 'ph_up' || rele == 'npk' || rele == 'micronutrientes' || rele == 'actuador de temperatura'){
             clientMqtt.publish('smartgrow/hidroponico/actuadores', rele);
-        } else {
+        } else if (rele == 'recirculacion_hidroponico') {
             clientMqtt.publish('smartgrow/hidroponico/actuadores', rele);
             setTimeout(() => {
                 clientMqtt.publish('smartgrow/hidroponico/actuadores', rele);
